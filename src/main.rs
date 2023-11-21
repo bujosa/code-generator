f48d130226ba3af35c7589684e582f6dc4093b00
bujosa <davidbujosa@gmail.com>
2023-11-21 00:52:21 -0400

    feat: create post method for the QR

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
 use qrcode_generator::QrCodeEcc;
 use std::io::Read;
 
-async fn generate_qr() -> impl Responder {
+#[derive(serde::Deserialize)]
+struct QrRequest {
+    text: String,
+}
+
+#[post("/qr")]
+async fn generate_qr(info: web::Json<QrRequest>) -> impl Responder {
     let file_path = "tmp/file_output.png";
-    qrcode_generator::to_png_to_file("Hello world!", QrCodeEcc::Low, 1024, file_path).unwrap();
+    qrcode_generator::to_png_to_file(&info.text, QrCodeEcc::Low, 1024, file_path).unwrap();
 
     let mut file = std::fs::File::open(file_path).unwrap();
     let mut buffer = Vec::new();
@@ -15,7 +21,7 @@ async fn generate_qr() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/qr", web::get().to(generate_qr)))
+    HttpServer::new(|| App::new().service(generate_qr))
         .bind("127.0.0.1:8080")?
         .run()
         .await
