113497f1dfa3b0618ec3bb73ce934963c01dfa18
bujosa <davidbujosa@gmail.com>
2023-11-21 00:37:36 -0400

    feat: add examp[le using qrcode generator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    use qrcode_generator::QrCodeEcc;
+
+    qrcode_generator::to_png_to_file("Hello world!", QrCodeEcc::Low, 1024, "tmp/file_output.png")
+        .unwrap();
 }
